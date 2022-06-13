6613e60468c5cb770a95dea4d4a01e4e818a25df
unknown <jimmyale3102@gmail.com>
2022-06-13 15:31:13 -0500

    Show next holiday

--- app/src/main/java/dev/alejo/colombiaholidays/ui/view/MainActivity.kt
+++ app/src/main/java/dev/alejo/colombiaholidays/ui/view/MainActivity.kt
@@ -1,20 +1,31 @@
 package dev.alejo.colombiaholidays.ui.view
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
+import dev.alejo.colombiaholidays.R
+import dev.alejo.colombiaholidays.core.Constants.Companion.CODE_200_RESPONSE
 import dev.alejo.colombiaholidays.core.lightStatusBar
 import dev.alejo.colombiaholidays.core.setFullScreen
+import dev.alejo.colombiaholidays.data.model.HolidayModel
 import dev.alejo.colombiaholidays.databinding.ActivityMainBinding
 import dev.alejo.colombiaholidays.ui.viewmodel.HolidayViewModel
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
+@SuppressLint("SimpleDateFormat")
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel: HolidayViewModel by viewModels()
+    private val dateFormat by lazy { SimpleDateFormat("EEE, MMM dd") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +43,7 @@ class MainActivity : AppCompatActivity() {
             holidays.forEach { it -> println(it.localName) }
         }
         viewModel.nextPublicHolidayResponse.observe(this) { holiday ->
-            println("*********************************************************")
-            println(holiday.toString())
+            runOnUiThread { showNextHoliday(holiday) }
         }
         viewModel.todayHolidayResponse.observe(this) { todayHoliday ->
             runOnUiThread { showTodayHoliday(todayHoliday) }
@@ -42,6 +52,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun showTodayHoliday(todayHoliday: String) {
         binding.holiday.text = todayHoliday
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showNextHoliday(nextHoliday: HolidayModel) {
+        val holidayDate = SimpleDateFormat("yyyy-MM-dd").parse(nextHoliday.date)
+        val dateFormatted = holidayDate?.let { date ->
+            dateFormat.format(date)
+        }
+        if(binding.holiday.text.isEmpty() || binding.holiday.text != CODE_200_RESPONSE) {
+            binding.holidayDescription.text = getString(R.string.upcoming_holiday) +
+                " $dateFormatted ${getString(R.string.next_holiday_for)} ${nextHoliday.localName}"
+        }
     }
 
     private fun initUI() {
